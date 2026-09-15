cefadebfc01aeb569730e0f436267ffaf7b1acb3
Shreya Patel <patelshreya.v.1806@gmail.com>
2026-09-15 16:43:26 +0530

    add feature

---
+++ p2.py
@@ -0,0 +1,15 @@
+print("================basic function====================")
+
+def greet():
+    print("hello")
+
+greet()
+
+print("================basic function====================")
+
+
+def greet(name):
+    print("hello",name)
+
+
+greet("shreya")
